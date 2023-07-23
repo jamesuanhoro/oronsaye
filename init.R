8c4f9f0ba682a056518af15bbf1697d8bb1ba7b4
James Uanhoro <jamesuanhoro@gmail.com>
2023-07-23 11:33:39 -0500

    heroku prep

---
+++ init.R
@@ -0,0 +1,12 @@
+# init.R
+
+helpers.installPackages(
+  "tools", "data.table", "shiny"
+)
+helpers.installPackages(
+  "BH"
+)
+install.packages("anytime_0.3.9.tar.gz", repos = NULL, type = "source")
+helpers.installPackages(
+  "shinyWidgets"
+)
