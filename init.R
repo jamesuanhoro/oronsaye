66d371e0f4503686065867f5b49072c6d7b7c1e3
James Uanhoro <jamesuanhoro@gmail.com>
2023-07-23 11:36:47 -0500

    her red

--- init.R
+++ init.R
@@ -1,10 +1,7 @@
 # init.R
 
 helpers.installPackages(
-  "tools", "data.table", "shiny"
-)
-helpers.installPackages(
-  "BH"
+  "tools", "shiny", "data.table", "BH"
 )
 install.packages("anytime_0.3.9.tar.gz", repos = NULL, type = "source")
 helpers.installPackages(
