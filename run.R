d8d0b7d950093f63e23ed57161ff5968f5cc940f
James Uanhoro <jamesuanhoro@gmail.com>
2023-07-23 11:05:38 -0500

    updated packages

--- run.R
+++ run.R
@@ -1,4 +1,13 @@
 # run.R
+
+my_packages <- c("data.table", "shiny", "shinyWidgets", "tools")
+install_if_missing <- function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+invisible(sapply(my_packages, install_if_missing))
+
 library(shiny)
 port <- Sys.getenv("PORT")
 shiny::runApp(
